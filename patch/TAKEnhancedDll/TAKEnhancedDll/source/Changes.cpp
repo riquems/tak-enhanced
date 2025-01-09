cfd2bde9413500b0dfc4eea2c8de9f9a33d8107e
Henrique Souza <Henrique-MS@Outlook.com>
2025-01-09 01:04:42 -0300

    Bump version to v1.3.5

--- patch/TAKEnhancedDll/TAKEnhancedDll/source/Changes.cpp
+++ patch/TAKEnhancedDll/TAKEnhancedDll/source/Changes.cpp
@@ -17,7 +17,7 @@
 #include "TAKCore/Config.h"
 #include <TAKEnhancedDll/Changes/FriendlyFire.hpp>
 
-__declspec(dllexport) const char* TAK_Enhanced_Label = "TA:K Enhanced v1.3.4";
+__declspec(dllexport) const char* TAK_Enhanced_Label = "TA:K Enhanced v1.3.5";
 
 void applyTakEnhancedVersion()
 {
